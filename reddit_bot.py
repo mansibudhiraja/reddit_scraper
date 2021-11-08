c711fd33cf589c3305f354babde6192adfa15e52
mansibudhiraja <58864151+mansibudhiraja@users.noreply.github.com>
2021-11-09 01:29:06 +0530

    Add logging and fix filter logic

--- reddit_bot.py
+++ reddit_bot.py
@@ -7,6 +7,25 @@ import os
 import argparse
 import sys
 import re
+import traceback
+import logging
+import logging.handlers as handlers
+import time
+
+logger = logging.getLogger('reddit')
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logHandler = handlers.TimedRotatingFileHandler('/tmp/normal.log', when='M', interval=1440, backupCount=0)
+logHandler.setLevel(logging.INFO)
+logHandler.setFormatter(formatter)
+
+errorLogHandler = handlers.RotatingFileHandler('/tmp/error.log', maxBytes=5000, backupCount=0)
+errorLogHandler.setLevel(logging.ERROR)
+errorLogHandler.setFormatter(formatter)
+
+logger.addHandler(logHandler)
+logger.addHandler(errorLogHandler)
 
 def get_json_from_url(url):
     response = requests.get(url)
@@ -23,7 +42,6 @@ def notify_telegram_group(form, subreddit, permalink, content):
     # extract chat_id for the group where you want to send the notifications from get_updates api call
     # all_messages = get_json_from_url(updates_url)
     # print(all_messages)
-    # chat_id = "-689248984"  #test ID
     chat_id = "-638366714"
     message = 'Type: {} \nsubreddit: {} \n\nurl: {} \n\ncontent: {}'.format(form, subreddit, url, content)
     message_url = "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(BOT_TOKEN, chat_id, message)
@@ -31,7 +49,7 @@ def notify_telegram_group(form, subreddit, permalink, content):
 
 
 def process_submission(all_posts):
-
+    unique_posts = {}
     if "title" in vars(all_posts):
         normalized_text = all_posts.title.lower()
         type_post ="submission"
@@ -42,7 +60,9 @@ def process_submission(all_posts):
     for word in search_list:
         pattern = r'\b' + word + r'\b'
         if re.search(pattern, normalized_text):
-            notify_telegram_group(type_post, all_posts.subreddit, all_posts.permalink, normalized_text)
+            if normalized_text not in unique_posts:
+                unique_posts[normalized_text] = 1
+                notify_telegram_group(type_post, all_posts.subreddit, all_posts.permalink, normalized_text)
     last_timestamp = datetime.now()
     set_last_sync_time(filename, last_timestamp)
     return
@@ -52,15 +72,19 @@ def get_submissions_and_comments(subreddit, **kwargs):
     results.extend(subreddit.new(**kwargs))
     results.extend(subreddit.comments(**kwargs))
     results.sort(key=lambda post: post.created_utc, reverse=True)
+    logger.info("Fetched %d submissions and comments", len(results)) 
+    if len(results)>0:
+        logger.info("First entry created timestamp %s", str(results[0]))
     return results
 
 
 def set_last_sync_time(filename, last_timestamp_datetime):
-    last_timestamp = last_timestamp_datetime.strftime('%Y-%m-%d %H:%M:%S')
-    details = {'last_timestamp': last_timestamp}
+    local_last_timestamp = last_timestamp_datetime.strftime('%Y-%m-%d %H:%M:%S')
+    details = {'last_timestamp': local_last_timestamp}
     file = open(filename, "w")
     file.write(json.dumps(details))
     file.close()
+    logger.info("Updated last sync time %s",local_last_timestamp)
 
 def get_last_sync_time(filename):
     if not os.path.exists(filename):
@@ -69,6 +93,7 @@ def get_last_sync_time(filename):
         data = file.read()
         js = json.loads(data)
         last_timestamp_datetime_format = datetime.strptime(js["last_timestamp"], '%Y-%m-%d %H:%M:%S')
+    logger.info("Fetched Last sync time from file: %s", str(last_timestamp_datetime_format))
     return last_timestamp_datetime_format
 
 
@@ -102,36 +127,30 @@ try:
         search_list = args.search_list
     except:
         e = sys.exc_info()
-        print(e)
-
+        logger.error(e)
 
 
     subreddit = reddit.subreddit(subreddit_list)
+    logger.info("Sucesssfully fetched subreddit")
     stream = praw.models.util.stream_generator(lambda **kwargs: get_submissions_and_comments(subreddit, **kwargs))
-    filename="last_sync_timestamp.txt"
+    logger.info("Fetched comments successfully")
+    path_current_directory = os.path.dirname(__file__)
+    filename = os.path.join(path_current_directory, 'last_sync_timestamp.txt')
 
     # set the last_sync_time for the ist run
     if not os.path.exists(filename):
         last_timestamp = datetime.now()
         set_last_sync_time(filename, last_timestamp)
+        logger.info("Successfully added last sync time for the first run")
 
 
     last_timestamp_dateformat = get_last_sync_time(filename)
     last_timestamp = last_timestamp_dateformat.replace(tzinfo=timezone.utc).timestamp()  ## convert to UTC
 
     for post in stream:
-        if last_timestamp > post.created_utc:
+        if last_timestamp < post.created_utc:
             process_submission(post)
 
 except Exception:
-    print(traceback.print_exc())
-
-
-
-
-
-
-
-
-
+    logger.error(traceback.print_exc())
 
